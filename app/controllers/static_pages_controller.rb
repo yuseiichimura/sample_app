70f33292491b3b84e9768317c85de9ef762ad018
Ichimura Yusei <ichimurayusei@ichimurayuuseinoMacBook-Air.local>
2020-12-28 17:57:13 +0900

    Make abasic User model (including secure passwords)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+  
 
-  def help
-  end
 
-  def about
+  def contact
   end
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+              password: "foobar", password_confirmation: "foobar")
+  end
+
+
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " *6
+    assert_not @user.valid?
+    
+  end
+
+  test "password" should have a minimum lengh" do
+    @user.password = `user.password_confirmation = "a" *5
+    assert_not @user.valid?
+  end
+end
